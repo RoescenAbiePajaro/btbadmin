d9f096beea50ce064e5fc8205784fcec9c5eb4b7
RoescenAbiePajaro <157484846+RoescenAbiePajaro@users.noreply.github.com>
2025-10-14 12:26:58 +0800

    update trackclick

--- backend/utils/trackClick.js
+++ backend/utils/trackClick.js
@@ -1,6 +1,6 @@
 export const trackClick = async (button, page) => {
     try {
-      await fetch("https://btbadmin2.onrender.com/api/clicks", {
+      await fetch("https://btbsitess.onrender.com/api/clicks", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ button, page }),
