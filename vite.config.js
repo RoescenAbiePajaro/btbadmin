ed7f8e1c2bcefdfdbfd919da0afeb35608d89f4b
RoescenAbiePajaro <157484846+RoescenAbiePajaro@users.noreply.github.com>
2026-06-05 23:45:46 +0800

    vite

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,8 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // This ensures assets are loaded from root
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'https://btbtestservice.onrender.com',
-        changeOrigin: true,
-        secure: false
-      }
-    }
-  }
 })
+
